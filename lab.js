4d8f1f69d227999aa29b0d2ce56648f15251453d
vayntail <113714157+vayntail@users.noreply.github.com>
2024-11-12 11:31:42 -0500

    Lab finished

--- lab.js
+++ lab.js
@@ -1,4 +1,4 @@
-//  Part 1: Stack Overflow
+// Part 1: Stack Overflow
 let count = 0;
 function increment() {
   count += 1;
@@ -12,12 +12,11 @@ try {
 }
 // when using recursive functions, always use the call inside try catch!
 
-//  Part 2: Trampolins
+// Part 2: Trampolins
 const flattenArr = (arr) => {
   let flat = [];
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] instanceof Array) {
-      console.log(flat);
       flat = flat.concat(flattenArr(arr[i]));
     } else {
       flat.push(arr[i]);
@@ -25,6 +24,33 @@ const flattenArr = (arr) => {
   }
   return flat;
 };
-
 const arr = ["hi", ["yes", "has", ["um", "3"]], "no"];
 console.log(flattenArr(arr));
+
+// Part 3: Deferred Execution
+const h1El = document.querySelector("h1");
+const primeNumbers = (n) => {
+  let count = 1;
+
+  const displayPrimes = () => {
+    if (count > n) {
+      alert("complete!");
+      return; // return when count hits n
+    } else {
+      setTimeout(() => {
+        if (count % 2 == 1) {
+          if (h1El.textContent == "") {
+            h1El.textContent += count; // if first time, don't add comma in first
+          } else {
+            h1El.textContent += ", " + count;
+          }
+        }
+        count++; // increment count
+        displayPrimes(); // call recursion
+      }, 1000);
+    }
+  };
+
+  displayPrimes(); // first call the function
+};
+primeNumbers(3);
